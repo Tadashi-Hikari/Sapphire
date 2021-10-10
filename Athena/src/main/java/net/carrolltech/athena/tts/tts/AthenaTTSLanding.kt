1e3f37023bf7b401835881dc0ea20654193c3c48
Christopher Carroll <ccarroll4812@gmail.com>
2021-10-10 15:42:53 -0400

    Annoyingly, I have to bounce an intent from the actial TTS serviec to a background one, since Android restricts the TTS to a single thread which causes tensorflow to run too slow. but it works now

---
+++ Athena/src/main/java/net/carrolltech/athena/tts/tts/AthenaTTSLanding.kt
@@ -0,0 +1,44 @@
+package net.carrolltech.athena.tts.tts
+
+import android.content.Intent
+import net.carrolltech.athena.framework.SapphireFrameworkService
+import net.carrolltech.athena.tts.dispatcher.OnTtsStateListener
+import net.carrolltech.athena.tts.dispatcher.TtsStateDispatcher
+
+class AthenaTTSLanding: SapphireFrameworkService() {
+    var speed = 1.0F
+    var ready = false
+    var queue = ""
+
+    override fun onCreate() {
+        super.onCreate()
+        TtsManager.getInstance().init(this)
+
+        TtsStateDispatcher.getInstance().addListener(object : OnTtsStateListener {
+            override fun onTtsReady() {
+                Log.d("The TTS is ready")
+                ready = true
+                TtsManager.getInstance().speak(queue, speed, true)
+            }
+            override fun onTtsStart(text: String) {
+            }
+            override fun onTtsStop() {
+                Log.d("The TTS has stopped")
+            }
+        })
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent!!.action == "on.the.backend") {
+            if(ready == true) {
+                TtsManager.getInstance().speak("this is a test", speed, true)
+            }else{
+                queue = intent.getStringExtra("payload")!!
+            }
+        }else if(intent!!.action == "stop.tts"){
+            TtsManager.getInstance().stopTts()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
